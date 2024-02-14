df6878b4cff1bdf66eccdebb152d4630a3b162b9
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-02-15 00:02:21 +0530

    - in main file we add @EnableConfigServer on class - in application.properties we add location of git repo with key "spring.cloud.config.server.git.uri" - for local git repo we do file://C://Users//----path-----

--- src/main/java/org/shiv/SpringCloudConfigServerApplication.java
+++ src/main/java/org/shiv/SpringCloudConfigServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
